926d0f08e22ba204698ae83c511fd8c969b74039
Jeong <jgjung12@gmail.com>
2023-08-25 16:21:02 +0900

    AlexNet Validation

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # learn-dl-py
 Python, Tensorflow
+
+"""
+# Model Hyperparameters
+NUM_FILTER = 64
+DROPOUT_RATE = 0.5
+
+# Learning Hyperparameters
+ITERATION = 1
+EPOCHS = 100
+BATCH_SIZE = 128
+OPTIMIZER = tf.keras.optimizers.Nadam
+LOSS_FUNCTION = "categorical_crossentropy"
+
+# Learning Scheduler
+INITIAL_LR = 0.1
+DECAY_RATE = 0.98
+LEARN_SCHEDULE = tf.keras.optimizers.schedules.ExponentialDecay
+"""
+
+"""
+AlexNet: 
+    Total params: 6,283,722 (23.97 MB)
+
+AlexNet_nb: 
+    Total params: 6,276,234 (23.94 MB)
+
+AlexNet_gloavg: 
+    Total params: 5,102,026 (19.46 MB)
+
+AlexNet_gelu: 
+    Total params: 6,283,722 (23.97 MB)
+
+AlexNet_gelu_gloavg: 
+    Total params: 5,102,026 (19.46 MB)
+
+AlexNet_res: 
+    Total params: 1,229,130 (4.69 MB)
+
+AlexNet_res_deep: 
+    Total params: 2,781,002 (10.61 MB)
+
+AlexNet_res_deep_deep: 
+    Total params: 4,332,874 (16.53 MB)
+"""
--- train.py
+++ train.py
@@ -2,84 +2,36 @@ import tensorflow as tf
 from keras import callbacks
 from dataset import load_cifar10, augment_image
 
-from CNN.AlexNet import AlexNet
-from CNN.AlexNet_nb import AlexNet_nb
-from CNN.AlexNet_gelu import AlexNet_gelu
-from CNN.AlexNet_gelu_gloavg import AlexNet_gelu_gloavg
-from CNN.AlexNet_gloavg import AlexNet_gloavg
-from CNN.AlexNet_res import AlexNet_res
-from CNN.AlexNet_res_deep import AlexNet_res_deep
-
-from CNN.DenseNet import DenseNet
-from CNN.ResNet import ResNet
-
-
-"""
-1. AlexNet
-2. ResNet
-3. DensNet
-"""
+from AlexNet.AlexNet import AlexNet
+from AlexNet.AlexNet_nb import AlexNet_nb
+from AlexNet.AlexNet_gelu import AlexNet_gelu
+from AlexNet.AlexNet_gelu_gloavg import AlexNet_gelu_gloavg
+from AlexNet.AlexNet_gloavg import AlexNet_gloavg
+from AlexNet.AlexNet_res import AlexNet_res
+from AlexNet.AlexNet_res_deep import AlexNet_res_deep
+from AlexNet.AlexNet_res_deep_deep import AlexNet_res_deep_deep
+from AlexNet.AlexNet_res_deep_deep_drop import AlexNet_res_deep_deep_drop
 
 # Model Save 이름 지정
-LOAD_MODEL = AlexNet_res_deep
-SAVE_NAME = "AlexNet_res_deep"
+
+LOAD_MODEL = AlexNet_res_deep_deep_drop
+SAVE_NAME = "AlexNet_res_deep_deep_drop_d_0.1"
 DATASET = "C10"
-# LOG_VERSION = ""
+LOG_VERSION = "d_0.3"
 
 # ChcekPoint, Log 경로 지정
 CHECKPOINT = f"checkpoints/{SAVE_NAME}_{DATASET}.h5"
-# LOG = f"./logs/{SAVE_NAME}_{DATASET}_{LOG_VERSION}_logs"
-LOG = f"./logs/{SAVE_NAME}_{DATASET}_logs"
+LOG = f"./logs/{SAVE_NAME}_{DATASET}_{LOG_VERSION}_logs"
+# LOG = f"./logs/{SAVE_NAME}_{DATASET}_logs"
 
 # DataSet Hyperparameters
 INPUT_SHAPE = (32, 32, 3)
 NUM_CLASSES = 10
 
-"""
-AlexNet:    Total params: 6,283,722 (23.97 MB)
-    # Model Hyperparameters
-    NUM_FILTER = 64
-    DROPOUT_RATE = 0.5
-
-    # Learning Hyperparameters
-    ITERATION = 1
-    EPOCHS = 100
-    BATCH_SIZE = 128
-    OPTIMIZER = tf.keras.optimizers.Nadam
-    LOSS_FUNCTION = "categorical_crossentropy"
-
-    # Learning Scheduler
-    INITIAL_LR = 0.1
-    DECAY_RATE = 0.98
-
-AlexNet_nb: Total params: 6,276,234 (23.94 MB)
-
-AlexNet_gloavg: Total params: 5,102,026 (19.46 MB)
-
-AlexNet_gelu: Total params: 6,283,722 (23.97 MB)
-
-AlexNet_gelu_gloavg: Total params: 5,102,026 (19.46 MB)
-
-AlexNet_res: Total params: 1,229,130 (4.69 MB)
-
-AlexNet_res_deep: Total params: 2781002 (10.61 MB)
-
-ResNet:
-    NUM_FILTER = 16
-    ITERATION = 1
-    EPOCHS = 400
-    BATCH_SIZE = 128
-
-DenseNet:
-    Growth_Rate = 12
-    ITERATION = 1
-    EPOCHS = 50
-    BATCH_SIZE = 256
-"""
-
 # Model Hyperparameters
 NUM_FILTER = 64
-DROPOUT_RATE = 0.5
+DROPOUT_RATE = 0.1
+COMPRESSION = 0.25
 
 # Learning Hyperparameters
 ITERATION = 1
@@ -109,7 +61,7 @@ model = LOAD_MODEL()._build(
     num_class=NUM_CLASSES,
     num_filter=NUM_FILTER,
     dropout_rate=DROPOUT_RATE,
-    compression=0.25,
+    compression=COMPRESSION,
 )
 model.summary()
 
